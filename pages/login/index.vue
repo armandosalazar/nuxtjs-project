74e221a692276eac7579c01559fcae6858325af5
Armando Salazar <armandosalazar351@gmail.com>
2024-03-05 02:07:46 -0600

    feat: add devDependencies and update login page

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,3 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+
+const email = ref('')
+
+</script>
+
 <template>
-    <n-input />
+    <n-flex justify="center">
+        <n-input v-model:value="email" type="text" placeholder="Email" />
+    </n-flex>
 </template>
