b16851e07938714483e1cfe13bd9ad834c4cd805
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2024-04-04 13:03:39 -0600

    feat: disable ssr /

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  routeRules: {
+    "/": { ssr: false }, // Disable server-side rendering
+  },
   devtools: { enabled: false },
   modules: [],
   // config tailwindcss
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
-      autoprefixer: {}
-    }
-  }
-})
+      autoprefixer: {},
+    },
+  },
+});
