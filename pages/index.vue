3a2cad150a271f1ab19d6a231c1ac012654af213
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2024-04-04 13:46:43 -0600

    feat: add last changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,10 @@
 <script setup>
-import {ref} from "vue";
+import { ref } from "vue";
 import Drawer from "../components/Drawer.vue";
 
 const count = ref(0);
 
 // @todo set ssr false by
-
 </script>
 
 <template>
@@ -17,7 +16,9 @@ const count = ref(0);
     <button @click="count++" class="bg-green-400 py-2 px-4 rounded font-bold">
       Count is: {{ count }}
     </button>
-    <br/>
+    <br />
     <a href="/options-api">Options API</a>
+    <br />
+    <a href="/login" class="text-green-400">Login</a>
   </main>
 </template>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { ref } from "vue";
+import { NInput } from "naive-ui";
 
-const email = ref("");
+const email = ref("hola");
 const password = ref("");
 
 console.log(email.value, password.value);
@@ -9,8 +10,21 @@ console.log(email.value, password.value);
 function handleKeydown(evt) {
   console.log(evt.key);
 }
+
+function handleLogin() {
+  console.log(email.value, password.value);
+}
 </script>
 
 <template>
-  <h1>Hi</h1>
+  <main class="container">
+    <n-input v-model="email" placeholder="Email" />
+    <n-input v-model="password" placeholder="Password" type="password" />
+    <button
+      @click="handleLogin"
+      class="bg-sky-500 py-2 px-4 rounded text-white font-bold mt-4"
+    >
+      Login
+    </button>
+  </main>
 </template>
